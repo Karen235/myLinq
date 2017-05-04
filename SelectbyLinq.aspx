9bda7a2cdef7a98f5f0732f85c9d03c485181dc0
KarenChen <karenc235@gmail.com>
2017-05-04 17:23:48 +0800

    aa

---
+++ SelectbyLinq.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SelectbyLinq.aspx.cs" Inherits="SelectbyLinq" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Select" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Insert" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delete" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SelectbyLinq.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SelectbyLinq : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack) return;
+
+       
+    }
+
+
+    private void GetData() {
+
+        DataClassesDataContext db = new DataClassesDataContext();
+        var person = from pa in db.Person
+                     where pa.Wid.Equals("010100")
+                     select pa;
+
+        foreach (var p in person)
+        {
+            Response.Write(p.Wid);
+            Response.Write(p.C_Name);
+            Response.Write(p.Birthday);
+        }
+    }
+
+
+
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        //update
+        DataClassesDataContext db = new DataClassesDataContext();
+        Person pa = db.Person.Single( per => per.Wid=="010100") ;
+        pa.Birthday = "1990/07/07";
+        pa.Cre_Usr = "010100";
+        pa.Cre_Date = DateTime.Now;
+        db.SubmitChanges();
+
+
+        GetData();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        GetData();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+
+        //insert
+        DataClassesDataContext db = new DataClassesDataContext();
+        Person pa = new Person();
+        pa.Wid = "A002";
+        pa.C_Name = "安德森";
+        pa.E_Name = "Anderson";
+        pa.Cre_Usr = "010100";
+        pa.Cre_Date = DateTime.Now;
+
+        db.Person.InsertOnSubmit(pa);
+        db.SubmitChanges();
+
+        //select
+        var person = from per in db.Person
+                     where per.Wid.Equals("A002")
+                     select per;
+
+        foreach (var p in person)
+        {
+            Response.Write(p.Wid);
+            Response.Write(p.C_Name);
+            Response.Write(p.Birthday);
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        DataClassesDataContext db = new DataClassesDataContext();
+        var person = from p in db.Person where p.Wid.Equals("A002") select p ;
+        db.Person.DeleteAllOnSubmit(person);
+        db.SubmitChanges();
+
+        //select
+        var personR = from per in db.Person
+                     where per.Wid.Equals("A002")
+                     select per;
+
+        foreach (var p in personR)
+        {
+            Response.Write(p.Wid);
+            Response.Write(p.C_Name);
+            Response.Write(p.Birthday);
+        }
+    }
+}
